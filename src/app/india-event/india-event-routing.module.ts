192927e62c7eee32e7076ae17339ebe141f56d15
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-03-03 13:56:10 +0530

    india-event child route added

--- src/app/india-event/india-event-routing.module.ts
+++ src/app/india-event/india-event-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndiaEventComponent } from './india-event.component';
+import { IndiaEventListComponent } from './india-event-list/india-event-list.component';
+import { IndiaEventDetailComponent } from './india-event-detail/india-event-detail.component';
 
 
 const indiaEventRoutes: Routes = [
-    { path: "", component: IndiaEventComponent }
+    { 
+        path: "", 
+        component: IndiaEventComponent,
+        children: [
+            {
+              path: '',
+              component: IndiaEventListComponent
+            },
+            {
+              path: 'detail',
+              component: IndiaEventDetailComponent
+            }
+          ]   
+    }
 ];
 
 @NgModule({
