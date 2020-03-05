fb06b97fc27cb0bacbe289734153fd8e0b0c5ba0
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-03-05 13:37:11 +0530

    roadrunners list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { RoadrunnersModule } from './roadrunners/roadrunners.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
