d5be6e2484c0383ff2ad8bb0ee5b5329e535606a
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-03-17 15:02:29 +0530

    Athlete fetched by Query

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { parseLazyRoute } from '@angular/compiler/src/aot/lazy_routes';
+import { __param } from 'tslib';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
@@ -13,6 +15,6 @@ export class UserService {
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
         headers.append('projectid', id);
-        return this.http.get('http://localhost:3000/user'+id)
+        return this.http.get('http://localhost:3000/user', { params: new HttpParams().set('user_id',  id) })
     }
 }
