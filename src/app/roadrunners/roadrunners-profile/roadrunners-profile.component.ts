d283b7f197d7febebf7f739c8f8ddcb361d22768
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-03-17 13:56:14 +0530

    added userId params to athlete-profile

--- src/app/roadrunners/roadrunners-profile/roadrunners-profile.component.ts
+++ src/app/roadrunners/roadrunners-profile/roadrunners-profile.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-roadrunners-profile',
   templateUrl: './roadrunners-profile.component.html',
   styleUrls: ['./roadrunners-profile.component.css']
 })
-export class RoadrunnersProfileComponent implements OnInit {
-
-  constructor() { }
+export class RoadrunnersProfileComponent implements OnInit, OnDestroy {
+  userId: number;
+  paramSubscription: Subscription;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.userId = +this.route.snapshot.params.id;
+    this.paramSubscription = this.route.params.subscribe(
+      (params: Params) => {
+      this.userId = +params.id;
+    })
+    console.log('user id' + this.userId);
   }
 
+  ngOnDestroy() {
+    this.paramSubscription.unsubscribe();
+  }
 }
