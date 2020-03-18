e23e5d79aa62636ca402247b70f6dc2e4e163b14
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-03-18 12:25:42 +0530

    all blogs fetch added in ts file

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../services/blog.service';
 
 @Component({
   selector: 'app-blogs',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogsComponent implements OnInit {
 
-  constructor() { }
+  allBlogs;
+  constructor(private blogsService: BlogService) { }
 
   ngOnInit() {
+    this.blogsService.fetchAllBlogs().subscribe( resData => {
+      this.allBlogs = resData;
+      console.log(this.allBlogs);
+    });
   }
 
 }
